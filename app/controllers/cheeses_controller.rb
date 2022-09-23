aed4e10a3d3a7c9bf5b9c1a939fe8bb0dd79b49c
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-09-23 00:51:16 -0400

    commit tree c25f5c7bbe8c63609774997cf150c157866c2d5a

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,12 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    # find a cheeses using ID from URL
+    # snd a JSON response using that cheese object
+    # byebug 
+    cheese = Cheese.find_by(id: params[:id])
+    render json: cheese
+  end
+
 end
